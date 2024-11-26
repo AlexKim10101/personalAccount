94d7aa40ae19c332b1c1c577af269a5be6c29354
Alex Kim <kim_alex@mail.ru>
2024-11-26 21:51:24 +0300

    menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,13 +10,19 @@ import {
 } from "@mui/material";
 import { Link } from "react-router";
 
-import { ReactComponent as MyIcon } from "../assets/icons/icon1.svg";
+import { ReactComponent as BurgerIcon } from "@assets/icons/burger-menu-icon.svg";
+import { ReactComponent as MyIcon } from "@assets/icons/icon1.svg";
+import { ReactComponent as LogoIcon } from "@assets/icons/logo_kpi.svg";
+// import myIconPath from "../assets/icons/icon1.svg";
 // import myIconPath from "..assets/icon";
 
 const pages = [
-	{ name: "Home", path: "/" },
-	{ name: "Login", path: "/login" },
-	{ name: "Account", path: "/account" },
+	{ name: "Главная", path: "/" },
+	{ name: "О продукте", path: "/login" },
+	{ name: "Кейсы", path: "/account" },
+	{ name: "Прайс и комплектации", path: "/" },
+	{ name: "Документация", path: "/login" },
+	{ name: "Контакты", path: "/account" },
 ];
 
 const Header: React.FC = () => {
@@ -32,69 +38,97 @@ const Header: React.FC = () => {
 	};
 
 	return (
-		<AppBar position="static">
-			<Toolbar>
-				{/* Логотип */}
+		<header>
+			{/* Иконка меню для мобильных */}
+			<Box sx={{ display: { xs: "flex", md: "none" } }}>
+				<IconButton
+					size="large"
+					edge="start"
+					color="inherit"
+					aria-label="menu"
+					onClick={handleMenuOpen}
+				>
+					<BurgerIcon />
+				</IconButton>
 
-				{/* Иконка меню для мобильных */}
-				<Box sx={{ display: { xs: "flex", md: "none" } }}>
-					<IconButton
-						size="large"
-						edge="start"
-						color="inherit"
-						aria-label="menu"
-						onClick={handleMenuOpen}
-					>
-						<MyIcon />
-					</IconButton>
-
-					{/* Мобильное меню */}
-					<Menu
-						anchorEl={anchorEl}
-						open={isMenuOpen}
-						onClose={handleMenuClose}
-						slotProps={{
-							paper: {
-								style: {
-									width: "200px",
-								},
+				{/* Мобильное меню */}
+				<Menu
+					anchorEl={anchorEl}
+					open={isMenuOpen}
+					onClose={handleMenuClose}
+					slotProps={{
+						paper: {
+							style: {
+								width: "200px",
 							},
-						}}
-					>
-						{pages.map(page => (
-							<MenuItem
-								key={page.name}
-								component={Link}
-								to={page.path}
-								onClick={handleMenuClose}
-							>
-								{page.name}
-							</MenuItem>
-						))}
-					</Menu>
-				</Box>
-
-				{/* Горизонтальное меню для Desktop */}
-				<Box sx={{ display: { xs: "none", md: "flex" } }}>
+						},
+					}}
+				>
 					{pages.map(page => (
-						<Typography
+						<MenuItem
 							key={page.name}
 							component={Link}
 							to={page.path}
-							sx={{
-								color: "inherit",
-								textDecoration: "none",
-								margin: "0 10px",
-								"&:hover": { textDecoration: "underline" },
-							}}
+							onClick={handleMenuClose}
+							style={{ margin: "0px", fontFamily: "inherit", display: "block" }}
 						>
 							{page.name}
-						</Typography>
+						</MenuItem>
 					))}
-				</Box>
-			</Toolbar>
-		</AppBar>
+				</Menu>
+			</Box>
+
+			{/* Горизонтальное меню для Desktop */}
+			<Box
+				sx={{
+					display: { xs: "none", md: "flex" },
+					gap: "12px",
+					alignItems: "center",
+				}}
+			>
+				<Typography
+					key="logo"
+					sx={{
+						display: "block",
+						color: "inherit",
+						textDecoration: "none",
+						margin: "0px",
+					}}
+				>
+					<LogoIcon />
+				</Typography>
+
+				{pages.map(page => (
+					<Typography
+						key={page.name}
+						component={Link}
+						to={page.path}
+						sx={{
+							display: "flex",
+							alignItems: "center",
+							color: "inherit",
+							fontFamily: "inherit",
+							textDecoration: "none",
+							margin: "0px",
+							"&:hover": { textDecoration: "underline" },
+						}}
+					>
+						{page.name}
+					</Typography>
+				))}
+			</Box>
+
+			<Link to="/login" className="link-to-login">
+				<div className="link-content">Попробовать</div>
+				<div className="link-content">
+					<MyIcon />
+				</div>
+			</Link>
+		</header>
 	);
 };
 
 export default Header;
+// element.style {
+
+// }
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -3,7 +3,7 @@ import Button from "@mui/material/Button";
 // import logo from './logo.svg';
 import "../index.css";
 import { Outlet } from "react-router";
-import Header from "../components/Header";
+import Header from "@components/Header";
 
 function App() {
 	return (
